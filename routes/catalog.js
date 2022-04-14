95920923728a8afe3c3e36947c8b583efd88215e
sri <sriyarocking98@gmail.com>
2022-04-14 16:49:23 +0530

    adding seneca code

---
+++ routes/catalog.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const seneca = require('seneca')();
+const entities = require('seneca-entity');
+const mongo_store = require('seneca-mongo-store');
+
+seneca
+  .use("entity")
+  .use('mongo-store', {
+    uri: 'mongodb://admin:India2020@172.30.20.143:27017/sampledb'
+  })
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/test',function(req, res, next) {
+    seneca.ready(function () {
+        var apple = seneca.make$('fruit')
+        apple.name  = 'Pink Lady'
+        apple.price = 0.99
+        apple.save$(function (err,apple) {
+          console.log( "apple.id = "+apple.id  )
+        })
+      })
+  })
+
+module.exports = router;
