ae17e509f651aa26e405b3d23a439bc2ac6db685
sriyancy <81805801+sriyancy@users.noreply.github.com>
2022-04-18 12:38:42 +0530

    Update catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// const seneca = require('seneca')();
-// const entities = require('seneca-entity');
-// const mongo_store = require('seneca-mongo-store');
+const seneca = require('seneca')();
+const entities = require('seneca-entity');
+const mongo_store = require('seneca-mongo-store');
 
-// seneca
-//   .use("entity")
-//   .use('mongo-store', {
+seneca
+  .use("entity")
+  .use('mongo-store', {
 
-//   uri :'mongodb://user1:user1@mongo:27017/checkconnect'
-//  // 172.30.66.141
-//   })
+  url :'mongodb://admin:India2020@172.30.75.153:27017/sampledb'
+ // 172.30.66.141
+  })
 
 
 
@@ -18,15 +18,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-// router.get('/test',function(req, res, next) {
-//     seneca.ready(function () {
-//         var apple = seneca.make$('fruit')
-//         apple.name  = 'Pink Lady'
-//         apple.price = 0.99
-//         apple.save$(function (err,apple) {
-//           console.log( "apple.id = "+apple.id  )
-//         })
-//       })
-//   })
+router.get('/test',function(req, res, next) {
+    seneca.ready(function () {
+        var apple = seneca.make$('fruit')
+        apple.name  = 'Pink Lady'
+        apple.price = 0.99
+        apple.save$(function (err,apple) {
+          console.log( "apple.id = "+apple.id  )
+        })
+      })
+  })
 
 module.exports = router;
